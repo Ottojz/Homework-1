93ee1d6cd75f25f34e7e22fb265607f4397c29e1
Вадим <ottojz@yandex.ru>
2024-02-10 13:12:03 +0300

    Initial commit Lesson branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var box = 5;
+        System.out.println(box);
+        box = box + 2;
+        System.out.println(box);
+        box = box - 3;
+        System.out.println(box);
+        box = box * 3;
+        System.out.println(box);
+        box = box / 4;
+        System.out.println(box);
+
+        var liftingCapacity = 50;
+        var staffWeight = 20;
+        var capacityLeft = liftingCapacity - staffWeight;
+        System.out.println("Еще можно положить " + capacityLeft + " кг вещей");
+
+        var appleWeight = 2;
+        var orangeWeight = 3;
+        var fruitWeight = appleWeight + orangeWeight;
+        System.out.println("Общий вес фруктов " + fruitWeight);
+
+        var meatWeight = 4;
+        var waterWeight = 5;
+        var tomatoesWeight = 2;
+        var cucumbersWeight = 2;
+        var peppersWeight = 2;
+        var zucchiniWeight = 2;
+        var vegetablesWeight = tomatoesWeight + cucumbersWeight + peppersWeight + zucchiniWeight;
+        var productsWeight = fruitWeight + vegetablesWeight + meatWeight + waterWeight;
+        System.out.println("Общий вес продуктов " + productsWeight + " кг!");
+
+        var leftWeight = liftingCapacity - staffWeight - productsWeight;
+        System.out.println("Места осталось " + leftWeight + " кг!");
+
+        productsWeight = productsWeight * 2;
+        System.out.println("Теперь вес продуктов " + productsWeight);
+
+        leftWeight = liftingCapacity - staffWeight - productsWeight;
+        System.out.println("Теперь места осталось " + leftWeight + " кг!");
+
+        //10:46
+
+        var overload = (staffWeight + productsWeight) % liftingCapacity;
+        System.out.println("Перегруз на " + overload + " кг!");
     }
 }
